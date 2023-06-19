2a797a206eb708b32e37cb83de576e52c6057ccd
Christine <christinekhekama99@gmail.com>
2023-06-19 19:48:40 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(ahmed, elion) {
+    const driver = elion.toLowerCase();
+    const matchingDrivers = ahmed.filter(ahmed =>
+      ahmed.toLowerCase().startsWith(driver)
+    );
+  
+    return matchingDrivers;
+  }
+  function fuzzyMatch(drivers, today) {
+    const matchingDrivers = drivers.filter(driver =>
+      driver.startsWith(today)
+    );
+  
+    return matchingDrivers;
+  }
+  function matchName(drivers, tee) {
+    const matchingDrivers = drivers.filter(driver =>
+      driver.name === tee
+    );
+  
+    return matchingDrivers;
+  }
